46f6824215eaf1c7f8238198d008f8818296d0c9
wiratmo <4a.wiratmo@gmail.com>
2024-10-16 10:06:35 +0700

    update surat permohonan

--- resources/views/pdf/components/header.blade.php
+++ resources/views/pdf/components/header.blade.php
@@ -7,50 +7,31 @@
 <div>
     <table class="table-auto">
         <tr>
-            <td rowspan="6" style="width: 120px;">
+            <td  style="width: 120px;">
                 <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('assets/logo/jateng-bw.png'))) }}"
                     alt="Logo" width="100" />
 
             </td>
-            <td style="text-align: center; line-height: 0.5;">
-                <span style="font-size: 10pt;">
+            <td style="text-align: center; line-height: 0.4;">
+                <p style="font-size: 10pt; ">
                     PEMERINTAH PROVINSI JAWA TENGAH
-                </span>
-            </td>
-        </tr>
-        <tr>
-            <td style="text-align: center; line-height: 0.5;">
-                <span style="font-size: 10pt;">
+                </p>
+                <p style="font-size: 10pt;">
                     DINAS PENDIDIKAN DAN KEBUDAYAAN
-                </span>
-            </td>
-        </tr>
-        <tr>
-            <td style="text-align: center; line-height: 0.5;">
-                <span style="font-weight: bold;">SEKOLAH MENENGAH KEJURUAN NEGERI 2 KARANGANYAR</span>
-            </td>
-        </tr>
-        <tr>
-            <td style="text-align: center; line-height: 10%;">
-                <span style="font-size: 10pt;">
+                </p>
+                <p style="font-weight: bold;">SEKOLAH MENENGAH KEJURUAN NEGERI 2 KARANGANYAR</p>
+                <p style="font-size: 10pt;">
                     Jalan Yos Sudarso, Kayangan Rt.01/Rw.04, Bejen, Karanganyar Kode Pos 57716
-                </span>
-            </td>
-        </tr>
-        <tr>
-            <td style="text-align: center; line-height: 10%;">
-                <span style="font-size: 10pt;">
+                </p>
+                <p style="font-size: 10pt;">
                     Telepon 0271- 494549/494335 Faximile 0271-6498171
-                </span>
-            </td>
-        </tr>
-        <tr>
-            <td style="text-align: center; line-height: 10%;">
-                <span style="font-size: 10pt;">
+                </p>
+                <p style="font-size: 10pt;">
                     Surat Elektronik smkn2kra97@gmail.com – website smkn2kra.sch.id
-                </span>
+                </p>
             </td>
         </tr>
     </table>
-    <hr>
+    <hr style="margin-top: -10px; border: 1.2px solid black;">
+    <hr style="margin-top: -6.5px; border: 0.1px solid black;">
 </div>
--- resources/views/pdf/permohonan.blade.php
+++ resources/views/pdf/permohonan.blade.php
@@ -6,116 +6,89 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Berkas Permohonan PKL</title>
-    @vite(['resources/css/app.css','resources/js/app.js', 'resources/js/dark.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/dark.js'])
 </head>
 
 <body>
     @include('pdf.components.header')
-    <div>
-        Nomor : 421.4/670/2024
-        Lamp : -
-        Perihal : Permohonan Tempat Praktik Kerja Lapangan ( PKL )
+    <div style="line-height: 0.8">
+        <p>Nomor    : 421.22</p>
+        <p>Nomor    : 421.22</p>
+        <p>Nomor    : 421.22</p>
+        <table border="1">
+            <tr>
+                <td colspan="2" style="width:-50px">Nomor</td>
+                <td style="width: 30px">:</td>
+                <td colspan="2">421.4/670/2024</td>
+            </tr>
+            <tr>
+                <td colspan="2" style="width:-50px">Lamp</td>
+                <td style="width: 30px">:</td>
+                <td colspan="2">-</td>
+            </tr>
+            <tr>
+                <td colspan="2" style="width:-50px">Perihal</td>
+                <td style="width: 30px">:</td>
+                <td colspan="2">Permohonan Tempat Praktik Kerja Lapangan ( PKL )</td>
+            </tr>
+            <tr>
+                <td style="width: 100px"></td>
+                <td colspan="4">
+                    <p>
+                        Kepada
+                        Yth. Pimpinan/Kepala
+                        Al Jadid Offset
+                        Jl. M Supeno, Tegalgede, Karanganyar
 
-        Kepada
-        Yth. Pimpinan/Kepala
-        Al Jadid Offset
-        Jl. M Supeno, Tegalgede, Karanganyar
-
-        Dengan hormat,
-        Untuk memantapkan kemampuan siswa siswi Sekolah Menengah Kejuruan, khususnya di SMK Negeri 2 Karanganyar, siswa
-        siswi diwajibkan mengikuti praktik kerja lapangan. Adapun kegiatan tersebut dilaksanakan selama kurang lebih 6
-        bulan. Untuk menunjang kegiatan tersebut kami mohon bantuan Bapak/Ibu untuk memberikan kesempatan kepada siswa
-        kami guna melaksanakan praktik kerja lapangan di tempat yang Bapak/Ibu pimpin pada tanggal 02 Oktober 2024
-        sampai dengan 28 Maret 2025.
-        Adapun data siswa yang kami kirim adalah sebagai berikut :
-
-        No Nama Siswa NIS Kelas Kompetensi Keahlian
-        1
-        2
-        GALANG KRISTANTO
-        IBNU RIDWAN
-
-
-
-
-        8575
-        8577
-
-
-
-
-
-
-        RPL A
-        RPL A
-
-
-
-
-
-
-        Rekayasa Perangkat Lunak
-
-
-        Demikian permohonan yang kami buat atas peran serta dan kerjasama yang baik kami mengucapkan terima kasih
-
-
-        Karanganyar, 01 Oktober 2024
-
-        Kepala SMK Negeri 2 Karanganyar
-        Kabupaten Karanganyar
-
-
-
-
-        Drs. Soetanto, M.T
-        Pembina Tk. I
-        NIP. 19661029 199802 1 002
-
-        SURAT BALASAN DARI DU/DI
-
-
-        Kepada
-        Yth.KEPALA SMK NEGERI 2 KARANGANYAR
-        Jl. Yos Sudarso Jengglong Bejen Telp. (0271) 494549
-        Karanganyar
-
-
-        Dengan hormat,
-        Sehubungan dengan Surat Permohonan Tempat Praktik Kerja Lapangan (PKL) dengan nomor 421.4/
-        670/2024 tertanggal 01 Oktober 2024 maka kami dari pihak Al Jadid Offset, Jl. M Supeno, Tegalgede, Karanganyar
-        menyatakan :
-
-        ( Menerima / Tidak Menerima )
-
-        Siswa/siswi tersebut untuk melaksanakan PKL pada tanggal 02 Oktober 2024 sampai dengan 28 Maret 2025, dengan
-        ketentuan siswa/siswi tersebut harus mentaati segala peraturan atau tatatertib yang berlaku di
-        Perusahaan/Bengkel/Instansi.
-        Adapun siswa/siswi yang kami terima sebagai berikut:
-
-        No Nama Siswa NIS Kelas Kompetensi keahlian
-        1
-        2
-        3
-        4
-        5
-        6
-        7
-        8
-        9
-        10
-
-        Demikian surat keterangan ini kami buat semoga dapat dimaklumi dan dapat dipergunakan sebagaimana mestinya.
-        NB. :
-        *) coret yang tidak perlu
-
-        ……………………….,..........................2024
-        Pimpinan DU/DI
-        ………………………….…..
-
-
-
-        (…………………….……….)
+                        Dengan hormat,
+                        Untuk memantapkan kemampuan siswa siswi Sekolah Menengah Kejuruan, khususnya di SMK Negeri 2
+                        Karanganyar, siswa
+                        siswi diwajibkan mengikuti praktik kerja lapangan. Adapun kegiatan tersebut dilaksanakan selama
+                        kurang lebih 6
+                        bulan. Untuk menunjang kegiatan tersebut kami mohon bantuan Bapak/Ibu untuk memberikan
+                        kesempatan kepada siswa
+                        kami guna melaksanakan praktik kerja lapangan di tempat yang Bapak/Ibu pimpin pada tanggal 02
+                        Oktober 2024
+                        sampai dengan 28 Maret 2025.
+                        Adapun data siswa yang kami kirim adalah sebagai berikut :
+                    </p>
+                    <table border="1">
+                        <tr>
+                            <th>No.</th>
+                            <th>Nama Siswa</th>
+                            <th>NIS</th>
+                            <th>Kelas</th>
+                            <th>Konsentrasi Keahlian</th>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>GALANG KRISTANTO</td>
+                            <td>8575</td>
+                            <td>XII RPL A</td>
+                            <td>Rekayasa Perangkat Lunak</td>
+                        </tr>
+                    </table>
+                    <p>
+                        Demikian permohonan yang kami buat atas peran serta dan kerjasama yang baik kami mengucapkan terima kasih
+                    </p>
+                </td>
+            </tr>
+            <br>
+            <tr>
+                <td colspan="4"></td>
+                <td>
+                    <p>Karanganyar, 01 Oktober 2024</p>
+                    <p>Kepala SMK Negeri 2 Karanganyar</p>
+                    <p>Kabupaten Karanganyar</p>
+                    <br>
+                    <br>
+                    <br>
+                    <p>Drs. Soetanto, M.T</p>
+                    <p>Pembina Tk. I</p>
+                    <p>NIP. 19661029 199802 1 002</p>
+                </td>
+            </tr>
+        </table>
 
 
 
