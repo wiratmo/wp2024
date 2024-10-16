5e9daccfb189f48667de2e440ed9f8194743b8e3
wiratmo <4a.wiratmo@gmail.com>
2024-10-16 11:42:04 +0700

    update surat permohonan

--- resources/views/pdf/permohonan.blade.php
+++ resources/views/pdf/permohonan.blade.php
@@ -6,61 +6,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Berkas Permohonan PKL</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/dark.js'])
+    <style>
+        .page-break {
+            page-break-after: always;
+        }
+    </style>
 </head>
 
-<body>
-    @include('pdf.components.header')
-    <div style="line-height: 0.8">
-        <p>Nomor    : 421.22</p>
-        <p>Nomor    : 421.22</p>
-        <p>Nomor    : 421.22</p>
-        <table border="1">
+<body>@include('pdf.components.header')
+    <div style="line-height: 1.2">
+        <table>
             <tr>
-                <td colspan="2" style="width:-50px">Nomor</td>
-                <td style="width: 30px">:</td>
-                <td colspan="2">421.4/670/2024</td>
+                <td>Nomor</td>
+                <td style="width:25px;text-align: center;">:</td>
+                <td colspan="3">421.4/670/2024</td>
             </tr>
             <tr>
-                <td colspan="2" style="width:-50px">Lamp</td>
-                <td style="width: 30px">:</td>
-                <td colspan="2">-</td>
+                <td>Lamp</td>
+                <td style="width:25px;text-align: center;">:</td>
+                <td colspan="3">-</td>
             </tr>
             <tr>
-                <td colspan="2" style="width:-50px">Perihal</td>
-                <td style="width: 30px">:</td>
-                <td colspan="2">Permohonan Tempat Praktik Kerja Lapangan ( PKL )</td>
+                <td>Perihal</td>
+                <td style="width:25px;text-align: center;">:</td>
+                <td colspan="3">Permohonan Tempat Praktik Kerja Lapangan (PKL)</td>
             </tr>
             <tr>
-                <td style="width: 100px"></td>
+                <td style="width: 80px"></td>
                 <td colspan="4">
-                    <p>
-                        Kepada
-                        Yth. Pimpinan/Kepala
-                        Al Jadid Offset
-                        Jl. M Supeno, Tegalgede, Karanganyar
-
-                        Dengan hormat,
-                        Untuk memantapkan kemampuan siswa siswi Sekolah Menengah Kejuruan, khususnya di SMK Negeri 2
-                        Karanganyar, siswa
-                        siswi diwajibkan mengikuti praktik kerja lapangan. Adapun kegiatan tersebut dilaksanakan selama
-                        kurang lebih 6
-                        bulan. Untuk menunjang kegiatan tersebut kami mohon bantuan Bapak/Ibu untuk memberikan
-                        kesempatan kepada siswa
-                        kami guna melaksanakan praktik kerja lapangan di tempat yang Bapak/Ibu pimpin pada tanggal 02
-                        Oktober 2024
-                        sampai dengan 28 Maret 2025.
-                        Adapun data siswa yang kami kirim adalah sebagai berikut :
+                    <p>Kepada <br>Yth. Pimpinan/Kepala <br><b>Al Jadid Offset</b><br>Jl. M Supeno,
+                        Tegalgede,
+                        Karanganyar <br></p>
+                    <p><br>Dengan hormat,
+                        <br>
                     </p>
-                    <table border="1">
+                    <p style="text-align: justify;">Untuk memantapkan kemampuan siswa siswi Sekolah Menengah Kejuruan,
+                        khususnya di SMK Negeri 2 Karanganyar,
+                        siswa siswi diwajibkan mengikuti praktik kerja lapangan. Adapun kegiatan tersebut dilaksanakan
+                        selama kurang lebih 6 bulan. Untuk menunjang kegiatan tersebut kami mohon bantuan Bapak/Ibu
+                        untuk memberikan kesempatan kepada siswa kami guna melaksanakan praktik kerja lapangan di tempat
+                        yang Bapak/Ibu pimpin pada tanggal <b>02 Oktober 2024 sampai dengan 28 Maret 2025</b>. Adapun
+                        data siswa yang kami kirim adalah sebagai berikut : </p>
+                    <table border="1" style="margin-left: 20px;">
                         <tr>
-                            <th>No.</th>
-                            <th>Nama Siswa</th>
-                            <th>NIS</th>
-                            <th>Kelas</th>
-                            <th>Konsentrasi Keahlian</th>
+                            <th style="width: 15px;">No.</th>
+                            <th style="width: 230px;">Nama Siswa</th>
+                            <th style="width: 50px;">NIS</th>
+                            <th style="width: 80px;">Kelas</th>
+                            <th style="width: 150px;">Konsentrasi Keahlian</th>
                         </tr>
-                        <tr>
+                        <tr style="text-align: center;">
                             <td>1</td>
                             <td>GALANG KRISTANTO</td>
                             <td>8575</td>
@@ -68,30 +63,49 @@
                             <td>Rekayasa Perangkat Lunak</td>
                         </tr>
                     </table>
-                    <p>
-                        Demikian permohonan yang kami buat atas peran serta dan kerjasama yang baik kami mengucapkan terima kasih
-                    </p>
+                    <p>Demikian permohonan yang kami buat atas peran serta dan kerjasama yang baik kami mengucapkan
+                        terima kasih </p>
                 </td>
-            </tr>
-            <br>
+            </tr><br>
             <tr>
                 <td colspan="4"></td>
                 <td>
-                    <p>Karanganyar, 01 Oktober 2024</p>
-                    <p>Kepala SMK Negeri 2 Karanganyar</p>
-                    <p>Kabupaten Karanganyar</p>
-                    <br>
-                    <br>
-                    <br>
-                    <p>Drs. Soetanto, M.T</p>
-                    <p>Pembina Tk. I</p>
-                    <p>NIP. 19661029 199802 1 002</p>
+                    <p style="line-height: 0.5;">Karanganyar,
+                        01 Oktober 2024</p>
+                    <p style="line-height: 0.5;">Kepala SMK Negeri 2 Karanganyar</p>
+                    <p style="line-height: 0.5;">Kabupaten Karanganyar</p><br><br><br><br>
+                    <p style="line-height: 0.5;"><b>Drs. Soetanto,
+                            M.T</b></p>
+                    <p style="line-height: 0.5;">Pembina Tk. I</p>
+                    <p style="line-height: 0.5;">NIP. 19661029 199802 1 002</p>
                 </td>
             </tr>
         </table>
+    </div>
+    <div class="page-break"></div>
+    <div>
+        <h2 style="text-align: center;">SURAT BALASAN DARI DU/DI</h2>
 
+        <P>
+            Kepada <br>
+            Yth.KEPALA SMK NEGERI 2 KARANGANYAR <br>
+            Jl. Yos Sudarso Jengglong Bejen Telp. (0271) 494549 <br>
+            Karanganyar <br>
+        </P>
+        <P>
+            Dengan hormat,<br>
+            Sehubungan dengan Surat Permohonan Tempat Praktik Kerja Lapangan (PKL) dengan nomor 421.4/
+            670/2024 tertanggal 01 Oktober 2024 maka kami dari pihak <b>Al Jadid Offset, Jl. M Supeno, Tegalgede,
+                Karanganyar</b> menyatakan :
 
+        <h3 style="text-align: center;"><b>( Menerima / Tidak Menerima )</b></h3>
 
+        Siswa/siswi tersebut untuk melaksanakan PKL pada tanggal 02 Oktober 2024 sampai dengan 28 Maret 2025, dengan
+        ketentuan siswa/siswi tersebut harus mentaati segala peraturan atau tatatertib yang berlaku di
+        Perusahaan/Bengkel/Instansi.
+        Adapun siswa/siswi yang kami terima sebagai berikut:
+
+        </P>
     </div>
 </body>
 
