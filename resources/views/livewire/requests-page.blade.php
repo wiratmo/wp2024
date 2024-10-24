406847b202b137c2b478154e1f32c681370b8758
wiratmo <4a.wiratmo@gmail.com>
2024-10-24 15:51:01 +0700

    update request prosess

--- resources/views/livewire/requests-page.blade.php
+++ resources/views/livewire/requests-page.blade.php
@@ -513,15 +513,15 @@
                                 </li>
                                 <li class="ms-6">
                                     @if ($item->status == 'accepted')
-                                        <span
-                                            class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -start-4 ring-4 ring-gray-400 dark:ring-gray-900 dark:bg-green-900">
-                                            <svg class="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                                                <path stroke="currentColor" stroke-linecap="round"
-                                                    stroke-linejoin="round" stroke-width="2"
-                                                    d="M1 5.917 5.724 10.5 15 1.5" />
-                                            </svg>
-                                        </span>
+                                    <span
+                                    class="absolute flex items-center justify-center w-8 h-8 bg-green-600 rounded-full -start-4 ring-4 ring-green-600 dark:ring-green-600 dark:bg-green-900">
+                                    <svg class="w-3.5 h-3.5 text-green-100 dark:text-green-400" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                        <path stroke="currentColor" stroke-linecap="round"
+                                            stroke-linejoin="round" stroke-width="2"
+                                            d="M1 5.917 5.724 10.5 15 1.5" />
+                                    </svg>
+                                </span>
                                     @elseif ($item->status == 'rejected')
                                         <span
                                             class="absolute flex items-center justify-center w-8 h-8 bg-red-800 rounded-full -start-4 ring-4 ring-red-800 dark:ring-gray-900 dark:bg-red-900">
