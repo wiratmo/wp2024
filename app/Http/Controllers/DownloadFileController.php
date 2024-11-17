428b3dbf75689224051e01f1590160be8d7e0a30
wiratmo <4a.wiratmo@gmail.com>
2024-11-17 12:07:13 +0700

    Updated: cetak surat permohonan

--- app/Http/Controllers/DownloadFileController.php
+++ app/Http/Controllers/DownloadFileController.php
@@ -2,21 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Student;
+
 use App\Models\User;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class DownloadFileController extends Controller
 {
-    public function _permohonan()
+    public function _permohonan(Request $request)
     {
-        $id_user = Auth::user()->id;
-        $user = User::studentSubmission($id_user);
-        // dd($user);
-        $pdf = Pdf::loadView('pdf.permohonan');
+        $industryId = $request->industryId;
+        $user = User::studentSubmission($industryId);
 
+        $pdf = Pdf::loadView('pdf.permohonan', compact("user"));
 
         return $pdf->stream('download.pdf');
     }
